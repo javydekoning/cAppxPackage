d1a7fcc481144f754bfd001befdeead0fd3ef5bc
Javy de Koning <jdekoning@ipsoft.com>
2016-07-30 14:27:33 +0200

    Splitting AppVeyor Scripts

---
+++ AppVeyor/AppVeyorInstall.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host 'Running AppVeyor install script'
+Write-Host 'Configuring NuGet and Installing Pester:'
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name Pester -Repository PSGallery -Force
+
+ls env: | out-string
