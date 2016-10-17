d6a3a99a842880efa77a522cad802737711e314b
Javy de Koning <javydekoning+github@gmail.com>
2016-10-17 18:42:52 +0200

    Update cAppxPackage.Tests.ps1
    Removed Re-curse from test file. No need to test the tests :).

--- Tests/cAppxPackage.Tests.ps1
+++ Tests/cAppxPackage.Tests.ps1
@@ -1,5 +1,4 @@
-﻿$Scripts = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.ps1’ -Recurse | Where-Object {$_.name -NotMatch ‘Tests.ps1’}
-$Modules = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.psm1’ -Recurse
+$Modules = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.psm1’
 $Rules   = Get-ScriptAnalyzerRule
 
 if ($Modules.count -gt 0) {
@@ -9,21 +8,6 @@ if ($Modules.count -gt 0) {
         foreach ($rule in $rules) {
           It “passes the PSScriptAnalyzer Rule $rule“ {
             (Invoke-ScriptAnalyzer -Path $module.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
-          }
-        }
-      }
-    }
-  }
-}
-if ($Scripts.count -gt 0) {
-  Describe ‘Testing all Script against default PSScriptAnalyzer rule-set’ {
-    foreach ($Script in $scripts) {
-      Context “Testing Script '$($script.FullName)'” {
-        foreach ($rule in $rules) {
-          It “passes the PSScriptAnalyzer Rule $rule“ {
-            if (-not ($module.BaseName -match 'AppVeyor') -and -not ($rule.Rulename -eq 'PSAvoidUsingWriteHost') ) {
-              (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
-            }
           }
         }
       }
